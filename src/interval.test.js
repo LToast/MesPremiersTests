4d5c3816e94dc6d83cc6e0b530127c27d9494526
lToast <theo.peuckert@gmail.com>
2019-03-05 17:10:48 +0100

    ajouts tests

--- src/interval.test.js
+++ src/interval.test.js
@@ -8,6 +8,7 @@ test('1-3 et 2-4 intersect', () => {
 test.each(
     [[1, 3, 2,4, true],
         [2, 4, 1,3, true],
+        [2, 3, 2,3, true],
         [0, 3, 3,6, false],
         [-1, 8, 6,4, true]])(
     'interval(%i, %i) intersept interval (%i,%i) %p',
@@ -20,8 +21,24 @@ test.each(
     [[1, 3, 2,4, false],
         [2, 6, 1,3, false],
         [2, 6, 2,3, false],
+        [2, 3, 2,3, false],
         [0, 3, 3,6, false],
         [-1, 8, 6,4, true]])(
+    'interval(%i, %i) includes interval (%i,%i) %p',
+    (a, b,c,d, expected) => {
+        expect((new Interval(a,b)).includes(new Interval(c,d))).toBe(expected);
+    },
+);
+//union
+test.each(
+    [[1, 3, 2,4, [1,2,3,4]],
+        [2, 6, 1,3, [1,2,3,4,5,6]],
+        [2, 6, 2,3, [2,3,4,5,6]],
+        [2, 3, 2,3, [2,3]],
+        [1, 2, 4,5, [1,2,4,5]],
+        [0, 3, 3,6, [0,1,2,3,4,5,6]],
+        [0, 3, 5,6, [0,1,2,3,5,6]],
+        [-1, 8, 6,4, [-1,0,1,2,3,4,5,6,7,8]]])(
     'interval(%i, %i) includes interval (%i,%i) %p',
     (a, b,c,d, expected) => {
         expect((new Interval(a,b)).includes(new Interval(c,d))).toBe(expected);
