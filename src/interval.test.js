ed792b9b47c66a04dfe6f7c9d11678df08a9b9cf
lToast <theo.peuckert@gmail.com>
2019-03-05 16:36:23 +0100

    test interval

--- src/interval.test.js
+++ src/interval.test.js
@@ -1,6 +1,5 @@
 let Interval = require('./interval');
-//Factoriel
-
+//Interval
 test('1-3 et 2-4 intersect', () => {
     let int1 = new Interval(2,4);
     let int2 = new Interval(1,3);
@@ -9,6 +8,17 @@ test('1-3 et 2-4 intersect', () => {
 test.each(
     [[1, 3, 2,4, true],
         [2, 4, 1,3, true],
+        [0, 3, 3,6, false],
+        [-1, 8, 6,4, true]])(
+    'interval(%i, %i) intersept interval (%i,%i) %p',
+    (a, b,c,d, expected) => {
+        expect((new Interval(a,b)).overlaps(new Interval(c,d))).toBe(expected);
+    },
+);
+//includes
+test.each(
+    [[1, 3, 2,4, false],
+        [2, 6, 1,3, true],
         [0, 3, 3,6, false],
         [-1, 8, 6,4, true]])(
     'interval(%i, %i) intersept interval (%i,%i) %p',
